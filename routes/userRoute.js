eacc6ac50acf2806141d02ebb8890b21e4128891
Krushna Waghumbare <waghumbarekrushna143@gmail.com>
2024-06-17 22:43:59 +0530

    set user get route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,16 @@
 const express=require("express");
+const User = require('../models/User')
 const app=express();
 
 const router=express.Router();
 
-router.get('/',(req,res)=>{
-    console.log(req.body);
-    res.send("Hello World");
-    
-})
+router.get('/', async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching users' });
+    }
+  });
 
 module.exports=router;
